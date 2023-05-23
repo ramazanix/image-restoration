4e9fe04afb898e1cb7bda527fdf2145967e91543
ramazanix <ramazanui2002@gmail.com>
2023-05-23 07:04:38 +0300

    Create function to hash filenames

--- src/security.py
+++ src/security.py
@@ -1,4 +1,5 @@
 from passlib.context import CryptContext
+from hashlib import shake_256
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -10,3 +11,7 @@ def verify_password(raw_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(raw_password: str) -> str:
     return pwd_context.hash(raw_password)
+
+
+def hash_file_name(filename: str):
+    return shake_256(filename.encode()).hexdigest(8)
