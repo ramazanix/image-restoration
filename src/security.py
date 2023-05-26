57ba093b544bc690a237174d12eb82ea18c79681
ramazanix <ramazanui2002@gmail.com>
2023-05-26 04:11:36 +0300

    Add func to process images

--- src/security.py
+++ src/security.py
@@ -1,3 +1,4 @@
+import subprocess
 from passlib.context import CryptContext
 from hashlib import shake_256
 
@@ -15,3 +16,11 @@ def get_password_hash(raw_password: str) -> str:
 
 def hash_file_name(filename: str):
     return shake_256(filename.encode()).hexdigest(8)
+
+
+def process_images(file_dir: str):
+    user_id = file_dir.split('/')[-1]
+    commands = ['python', 'run.py', '--input_folder', file_dir, '--output_folder',
+                f'/data/user_images/{user_id}', '--GPU', '-1', '--with_scratch']
+    proc = subprocess.Popen(commands, cwd='/image-restoration/neural_link')
+    proc.wait()
