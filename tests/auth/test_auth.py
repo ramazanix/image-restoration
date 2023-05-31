b564b73abbf11a42d83aede4538ba16abd027d1c
ramazanix <ramazanui2002@gmail.com>
2023-05-31 23:52:46 +0300

    Refactor tests auth

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,18 +1,11 @@
 import pytest
 from pytest_schema import exact_schema
-from .schemas import success, error
+from .schemas import login_response, refresh_access_token_response
 from ..users.schemas import user
 from httpx import AsyncClient
 
 
-correct_cookies: list[str] = [
-    "access_token_cookie",
-    "csrf_access_token",
-    "csrf_refresh_token",
-    "refresh_token_cookie",
-]
-
-user_data = {"username": "Sam", "password": "sam_password"}
+user_data = {"username": "username", "password": "password"}
 
 
 @pytest.mark.asyncio
@@ -21,26 +14,20 @@ async def test_auth_not_existed_user(client: AsyncClient):
     Trying to authenticate not existed user
     """
     response = await client.post(
-        "/auth/login", json={"username": "not_exists", "password": "12345678"}
+        "/api/auth/login", data=user_data
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
 
 
 @pytest.mark.asyncio
-async def test_auth_existed_user(client: AsyncClient):
+async def test_auth_existed_user(client: AsyncClient, create_user):
     """
     Trying to authenticate existed user
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
+    response = await client.post("/api/auth/login", data=user_data)
     assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
+    assert exact_schema(login_response) == response.json()
 
 
 @pytest.mark.asyncio
@@ -48,118 +35,110 @@ async def test_auth_blank_body(client: AsyncClient):
     """
     Trying to authenticate user with incorrect blank data
     """
-    response = await client.post("/auth/login", json={})
+    response = await client.post("/api/auth/login", data={})
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
 async def test_auth_not_valid_data(client: AsyncClient):
     """
-    Trying to authenticate user with invalid data
+    Trying to authorize user with invalid data
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
     response = await client.post(
-        "/auth/login", json={"username": "Sam", "password": "123"}
+        "/api/auth/login", data={"username": "u", "password": "123"}
     )
+    assert response.status_code == 401
+
+    response = await client.post("/api/auth/login", data={"a": "b"})
     assert response.status_code == 422
 
+
+@pytest.mark.asyncio
+async def test_auth_short_username(client: AsyncClient):
+    """
+    Trying to authorize user with short username
+    """
     response = await client.post(
-        "/auth/login", json={"username": "Sam", "password": "12345678"}
+        "/api/auth/login", data={"username": "u", "password": "password"}
+    )
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_auth_short_password(client: AsyncClient):
+    """
+    Trying to authorize user with short password
+    """
+    response = await client.post(
+        "/api/auth/login", data={"username": "username", "password": "123"}
+    )
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_auth_invalid_creds(client: AsyncClient):
+    """
+    Trying to authorize user with invalid credentials
+    """
+    response = await client.post(
+        "/api/auth/login", data={"username": "user", "password": "password"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
 
     response = await client.post(
-        "/auth/login", json={"username": "1234", "password": "123456789"}
+        "/api/auth/login", data={"username": "username", "password": "password2"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
 
     response = await client.post(
-        "/auth/login", json={"username": "1234", "password": "sam_password"}
+        "/api/auth/login", data={"username": "user", "password": "password2"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
 
 
 @pytest.mark.asyncio
-async def test_refresh_access_token(client: AsyncClient):
+async def test_refresh_access_token(client: AsyncClient, create_user, authorize):
     """
     Trying to refresh access token
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
+    headers = {"Authorization": f'Bearer {authorize["refresh_token"]}'}
+    response = await client.post("/api/auth/refresh", headers=headers)
     assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    headers = {"X-CSRF-Token": response.cookies.get("csrf_refresh_token")}
-    response = await client.post("/auth/refresh", headers=headers)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-
-    response = await client.post("/auth/refresh")
-    assert response.status_code == 401
-    assert exact_schema(error) == response.json()
-    assert response.json().get("detail") == "Missing CSRF Token"
+    assert exact_schema(refresh_access_token_response) == response.json()
 
 
 @pytest.mark.asyncio
-async def test_logout(client: AsyncClient):
+async def test_refresh_access_token_unauthorized(client: AsyncClient):
+    """
+    Trying to refresh access token unauthorized
+    """
+    response = await client.post("/api/auth/refresh")
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_logout(client: AsyncClient, create_user, authorization_header):
     """
     Trying to log out
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-
-    response = await client.delete("/auth/logout")
+    response = await client.delete("/api/auth/logout")
     assert response.status_code == 401
-    assert exact_schema(error) == response.json()
-    assert response.json().get("detail") == "Missing CSRF Token"
 
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-    response = await client.delete("/auth/logout", headers=headers)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert len(response.cookies) == 0
+    response = await client.delete("/api/auth/logout", headers=authorization_header)
+    assert response.status_code == 204
 
 
 @pytest.mark.asyncio
-async def test_get_current_user(client: AsyncClient):
+async def test_get_current_user(client: AsyncClient, create_user, authorization_header):
     """
     Trying to get current user
     """
-    response = await client.get("/users/me")
+    response = await client.get("/api/users/me")
     assert response.status_code == 401
-    assert exact_schema(error)
 
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    response = await client.get("/users/me")
+    response = await client.get("/api/users/me", headers=authorization_header)
     assert response.status_code == 200
     assert exact_schema(user)
