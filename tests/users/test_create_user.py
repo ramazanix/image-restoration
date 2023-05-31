279df1c06649bb4b0d5083b8a21a5e7d338cd4c0
ramazanix <ramazanui2002@gmail.com>
2023-05-31 23:54:23 +0300

    Refactor tests for create user

--- tests/users/test_create_user.py
+++ tests/users/test_create_user.py
@@ -4,7 +4,7 @@ from pytest_schema import exact_schema
 from .schemas import user, users
 
 
-user_data = {"username": "Paul", "password": "paul_password"}
+user_data = {"username": "username", "password": "password"}
 
 
 @pytest.mark.asyncio
@@ -12,33 +12,26 @@ async def test_create_user(client: AsyncClient):
     """
     Trying to create user
     """
-    response = await client.post("/users", json=user_data)
+    response = await client.post("/api/users", json=user_data)
     assert response.status_code == 201
-
     assert exact_schema(user) == response.json()
-
     assert response.json().get("username") == user_data["username"]
 
 
 @pytest.mark.asyncio
-async def test_create_couple_users(client: AsyncClient):
+async def test_create_couple_users(client: AsyncClient, create_user):
     """
     Trying to create a couple of users
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
     response = await client.post(
-        "/users", json={"username": "Tom", "password": "tom_password"}
+        "/api/users", json={"username": "Tom", "password": "tom_password"}
     )
 
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == "Tom"
 
-    response = await client.get("/users")
+    response = await client.get("/api/users")
     assert response.status_code == 200
     assert len(response.json()) == 2
     assert exact_schema(users) == response.json()
@@ -49,7 +42,7 @@ async def test_create_blank_body(client: AsyncClient):
     """
     Trying to send request with blank body
     """
-    response = await client.post("/users", json={})
+    response = await client.post("/api/users", json={})
     assert response.status_code == 422
 
 
@@ -58,16 +51,16 @@ async def test_create_invalid_user_data(client: AsyncClient):
     """
     Trying to send invalid body content
     """
-    response = await client.post("/users", json={"a": "b"})
+    response = await client.post("/api/users", json={"a": "b"})
     assert response.status_code == 422
 
-    response = await client.post("/users", json={"username": ""})
+    response = await client.post("/api/users", json={"username": ""})
     assert response.status_code == 422
 
-    response = await client.post("/users", json={"password": ""})
+    response = await client.post("/api/users", json={"password": ""})
     assert response.status_code == 422
 
-    response = await client.post("/users", json={"username": "", "password": ""})
+    response = await client.post("/api/users", json={"username": "", "password": ""})
     assert response.status_code == 422
 
 
@@ -77,7 +70,7 @@ async def test_create_user_short_username(client: AsyncClient):
     Trying to create user with too short username
     """
     response = await client.post(
-        "/users", json={"username": "u", "password": "password"}
+        "/api/users", json={"username": "u", "password": "password"}
     )
     assert response.status_code == 422
 
@@ -88,7 +81,7 @@ async def test_create_user_long_username(client: AsyncClient):
     Trying to create user with too long username
     """
     response = await client.post(
-        "/users", json={"username": "me" * 21, "password": "password"}
+        "/api/users", json={"username": "me" * 21, "password": "password"}
     )
     assert response.status_code == 422
 
@@ -98,7 +91,9 @@ async def test_create_user_short_password(client: AsyncClient):
     """
     Trying to create user with too short password
     """
-    response = await client.post("/users", json={"username": "Alex", "password": "123"})
+    response = await client.post(
+        "/api/users", json={"username": "Alex", "password": "123"}
+    )
     assert response.status_code == 422
 
 
@@ -108,7 +103,7 @@ async def test_create_user_long_password(client: AsyncClient):
     Trying to create user with too long password
     """
     response = await client.post(
-        "/users", json={"username": "Alex", "password": "123" * 11}
+        "/api/users", json={"username": "Alex", "password": "123" * 11}
     )
     assert response.status_code == 422
 
@@ -118,7 +113,9 @@ async def test_create_user_short_fields(client: AsyncClient):
     """
     Trying to create user with too short username and password
     """
-    response = await client.post("/users", json={"username": "u", "password": "passwd"})
+    response = await client.post(
+        "/api/users", json={"username": "u", "password": "passwd"}
+    )
     assert response.status_code == 422
 
 
@@ -128,7 +125,7 @@ async def test_create_user_long_fields(client: AsyncClient):
     Trying to create user with too long username and password
     """
     response = await client.post(
-        "/users", json={"username": "Joe" * 10, "password": "passwd" * 6}
+        "/api/users", json={"username": "Joe" * 10, "password": "passwd" * 6}
     )
     assert response.status_code == 422
 
@@ -138,11 +135,11 @@ async def test_create_existing_user(client: AsyncClient):
     """
     Trying to create existing user
     """
-    response = await client.post("/users", json=user_data)
+    response = await client.post("/api/users", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == user_data["username"]
 
-    response = await client.post("/users", json=user_data)
+    response = await client.post("/api/users", json=user_data)
     assert response.status_code == 400
     assert response.json().get("detail") == "User already exists"
