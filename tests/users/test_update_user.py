6fca525a04172a0a55a8d6095ac52598af9143fb
ramazanix <ramazanui2002@gmail.com>
2023-05-31 23:54:51 +0300

    Refactor tests for update user

--- tests/users/test_update_user.py
+++ tests/users/test_update_user.py
@@ -2,222 +2,187 @@ import pytest
 from httpx import AsyncClient
 from pytest_schema import exact_schema
 from .schemas import user
-from ..auth.schemas import success, error
 
 
-user_data = {"username": "Alex", "password": "alex_password"}
-
-correct_cookies: list[str] = [
-    "access_token_cookie",
-    "csrf_access_token",
-    "csrf_refresh_token",
-    "refresh_token_cookie",
-]
+user_data = {"username": "username", "password": "password"}
 
 
 @pytest.mark.asyncio
-async def test_update_user_unauthorized(client: AsyncClient):
+async def test_update_user_unauthorized(client: AsyncClient, create_user):
     """
     Trying to update user without auth
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
     response = await client.patch(
-        f"/users/{user_data['username']}", json={"username": "not_Alex"}
+        f"/api/users/{user_data['username']}", json={"username": "not_User"}
     )
     assert response.status_code == 401
 
 
 @pytest.mark.asyncio
-async def test_update_user_authorized(client: AsyncClient):
+async def test_update_user_authorized(
+    client: AsyncClient, create_user, authorization_header
+):
     """
     Trying to update user with auth
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
     response = await client.patch(
-        "/users/Alex", json={"username": "not_Alex"}, headers=headers
+        "/api/users/username",
+        json={"username": "not_User"},
+        headers=authorization_header,
     )
     assert response.status_code == 200
     assert exact_schema(user) == response.json()
-    assert response.json().get("username") == "not_Alex"
+    assert response.json().get("username") == "not_User"
 
-    response = await client.get(f"/users/{user_data['username']}", headers=headers)
+    response = await client.get(
+        f"/api/users/{user_data['username']}", headers=authorization_header
+    )
     assert response.status_code == 400
     assert response.json().get("detail") == "User not found"
 
-    response = await client.get("/users/not_Alex", headers=headers)
+    response = await client.get("/api/users/not_User", headers=authorization_header)
     assert response.status_code == 200
     assert exact_schema(user) == response.json()
-    assert response.json().get("username") == "not_Alex"
+    assert response.json().get("username") == "not_User"
 
 
 @pytest.mark.asyncio
-async def test_update_user_blank_body(client: AsyncClient):
+async def test_update_another_user(
+    client: AsyncClient, create_user, authorization_header
+):
+    """
+    Trying to update another user
+    """
+    response = await client.post(
+        "/api/users", json={"username": "user2", "password": "password"}
+    )
+    assert response.status_code == 201
+    assert exact_schema(user) == response.json()
+    assert response.json().get("username") == "user2"
+
+    response = await client.patch(
+        "/api/users/user2", json={"password": "12345678"}, headers=authorization_header
+    )
+    assert response.status_code == 405
+    assert response.json().get("detail") == "Method Not Allowed"
+
+
+@pytest.mark.asyncio
+async def test_update_user_blank_body(
+    client: AsyncClient, create_user, authorization_header
+):
     """
     Trying to update user with blank body
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
     response = await client.patch(
-        f"/users/{user_data['username']}", json={}, headers=headers
+        f"/api/users/{user_data['username']}", json={}, headers=authorization_header
     )
-
     assert response.status_code == 400
-    assert exact_schema(error) == response.json()
     assert response.json().get("detail") == "Bad Request"
 
 
 @pytest.mark.asyncio
-async def test_update_user_too_small_username(client: AsyncClient):
+async def test_update_user_too_small_username(
+    client: AsyncClient, create_user, authorization_header
+):
     """
     Trying to update user's username to small value
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
-    response = await client.patch("/users/Joe", json={"username": "J"}, headers=headers)
+    response = await client.patch(
+        "/api/users/username", json={"username": "J"}, headers=authorization_header
+    )
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
-async def test_update_user_too_long_username(client: AsyncClient):
+async def test_update_user_too_long_username(
+    client: AsyncClient, create_user, authorization_header
+):
     """
     Trying to update user's username to long value
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
     response = await client.patch(
-        "/users/Alex", json={"username": "A" * 21}, headers=headers
+        "/api/users/username", json={"username": "A" * 21}, headers=authorization_header
     )
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
-async def test_update_user_too_small_password(client: AsyncClient):
+async def test_update_user_too_small_password(
+    client: AsyncClient, create_user, authorization_header
+):
     """
     Trying to update user's password to small value
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
     response = await client.patch(
-        "/users/Alex", json={"password": "A" * 33}, headers=headers
+        "/api/users/username", json={"password": "A" * 33}, headers=authorization_header
     )
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
-async def test_update_user_too_long_password(client: AsyncClient):
+async def test_update_user_too_long_password(
+    client: AsyncClient, create_user, authorization_header
+):
     """
     Trying to update user's password to long value
     """
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == user_data["username"]
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
     response = await client.patch(
-        "/users/Alex", json={"password": "A" * 33}, headers=headers
+        "/api/users/username", json={"password": "A" * 33}, headers=authorization_header
     )
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
-async def test_update_user_invalid_body(client: AsyncClient):
+async def test_update_user_invalid_body(
+    client: AsyncClient, create_user, authorization_header
+):
     """
-    Trying to update user with invalid body
+    Trying to update user with invalid body keys
     """
-    user_data = {"username": "username", "password": "password"}
-    response = await client.post("/users", json=user_data)
-    assert response.status_code == 201
-    assert exact_schema(user) == response.json()
-    assert response.json().get("username") == "username"
-
-    response = await client.post("/auth/login", json=user_data)
-    assert response.status_code == 200
-    assert exact_schema(success) == response.json()
-    assert list(response.cookies.keys()) == correct_cookies
-
-    cookies = response.cookies
-    headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-
-    response = await client.patch("/users/username", json={"a": "b"}, headers=headers)
+    response = await client.patch(
+        "/api/users/username", json={"a": "b"}, headers=authorization_header
+    )
     assert response.status_code == 400
     assert response.json().get("detail") == "Bad Request"
 
+
+@pytest.mark.asyncio
+async def test_update_user_blank_username(
+    client: AsyncClient, create_user, authorization_header
+):
+    """
+    Trying to update user with blank username value
+    """
     response = await client.patch(
-        "/users/username", json={"username": "", "password": ""}, headers=headers
+        "/api/users/username", json={"username": ""}, headers=authorization_header
     )
     assert response.status_code == 422
 
+
+@pytest.mark.asyncio
+async def test_update_user_blank_password(
+    client: AsyncClient, create_user, authorization_header
+):
+    """
+    Trying to update user with blank password value
+    """
     response = await client.patch(
-        "/users/username",
+        "/api/users/username", json={"password": ""}, headers=authorization_header
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_update_user_username_blank_password(
+    client: AsyncClient, create_user, authorization_header
+):
+    """
+    Trying to update user with filled username value and blank password value
+    """
+    response = await client.patch(
+        "/api/users/username",
         json={"username": "username", "password": ""},
-        headers=headers,
+        headers=authorization_header,
     )
     assert response.status_code == 422
